75cff67a4ccf58a934bdbdc04d883e4e0f8558a9
Tony Kosturos <kosturosanthony@gmail.com>
2018-01-16 22:04:16 -0600

    finish adding in new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
    root                 'static_pages#home'
 
    get 'contact'     => 'static_pages#contact'
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
    get 'projects'    => 'static_pages#projects'
 
    get 'team'        => 'static_pages#team'
+
+   get 'signup'      => 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
