8f8267d2239482f3ba7aef69db9557bce63dfa99
Tony Kosturos <kosturosanthony@gmail.com>
2018-01-16 22:10:41 -0600

    finished end of chapter five excercises and all tests are currently passing

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
       assert_select "a[href=?]", contact_path
       assert_select "a[href=?]", team_path
       assert_select "a[href=?]", projects_path
+      get signup_path
+      assert_select "title", full_title("Sign up")
    end
 end
